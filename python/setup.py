d79826b8e9b0e0ac66d3eef8e8ddeb83c532e2bc
SimowAlaoui <alaoui.simo@gmail.com>
2017-09-05 16:06:02 +0200

    Release version 0.1 in pypi to test the lirary

---
+++ python/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'jpddsforcasting',
+  packages = ['jpddsforcasting'], 
+  version = '0.1',
+  description = 'Time Series forcasting pipelines library - Use different market models to forcast time series data',
+  author = 'Alaoui Mohamed',
+  author_email = 'alaoui.simo@gmail.com',
+  url = 'https://github.com/jetpackdata/jpdforcaster/tree/master/python/jpddsforcasting',
+  download_url = 'https://github.com/jetpackdata/jpdforcaster/archive/0.1.zip',
+  keywords = ['time series', 'forecasting', 'prophet', 'homology', 'machine learning'],
+  classifiers = [],
+)
